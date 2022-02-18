9b0403614f1dbfe1f39787df9d6543102d4689aa
Ashit Rai <nikkuashit@gmail.com>
2022-02-19 00:51:45 +0530

    allowed all host

--- hadoti_backend/settings.py
+++ hadoti_backend/settings.py
@@ -29,7 +29,8 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'hadoti.curiositydesk.com',
     'localhost:8000',
-    '*'
+    '*',
+    'http://localhost:4200',
 ]
 
 
@@ -63,10 +64,13 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS: True
+
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
 ]
+
 
 ROOT_URLCONF = 'hadoti_backend.urls'
 
